2d3e35c5f425e4312aec611d7bb743aee966188d
Jose Felipe Duarte Coronado <77589315+josefdc@users.noreply.github.com>
2023-09-14 14:36:42 +0000

    informacion y imagenes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,20 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Soy un estudiante de Machine Learning Research en la Universidad Tecnológica de Pereira (UTP). Me apasiona la aplicación de las matemáticas en el campo de la inteligencia artificial.";
+
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Aprendizaje profundo",
+  "Redes neuronales",
+  "Optimización matemática",
+  "Procesamiento de lenguaje natural",
+  "Visión por computadora",
+  "Modelos generativos",
 ];
 
 /**
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Me fascina descubrir cómo las matemáticas pueden dar vida a las máquinas, permitiéndoles aprender y adaptarse por sí mismas.";
 
 const About = () => {
   return (
